a165c95cc56da8fa2e2428f87cfacfac1d15ee98
Hugo-Olivier Monette <ak89420@ens.etsmtl.ca>
2018-06-21 15:24:00 -0400

    Finished Animations

--- src/App.vue
+++ src/App.vue
@@ -25,22 +25,45 @@ export default {
 <style>
 .fade-enter {
   /* opacity: 0; */
-  max-height: 0px;
+  /* max-height: 0px; */
 }
 
 .fade-enter-active {
-  transition: max-height 1s ease-in;
+  /* transition: max-height 1s ease-in;
   overflow: hidden;
-  max-height: 500px;
+  max-height: 500px; */
+  animation: grow 0.75s ease-out forwards;
+  overflow: hidden;
 }
 
 /* .fade-leave {
 } */
 
 .fade-leave-active {
-  transition: max-height 1s;
-  overflow: hidden;
-  max-height: 0px;
+  /* transition: max-height 1s;
+  overflow: hidden;  animation: grow 2s ease-out forwards;
+  max-height: 0px; */
   /* opacity: 0; */
+  animation: shrink 0.75s ease-out forwards;
+  overflow: hidden;
+}
+
+@keyframes grow {
+  from {
+    max-height: 0;
+    opacity: 0.25;
+  }
+  to {
+    max-height: 1000px;
+  }
+}
+@keyframes shrink {
+  from {
+    max-height: 1000px;
+  }
+  to {
+    max-height: 0;
+    opacity: 0.25;
+  }
 }
 </style>
